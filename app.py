e0c033093436f980678a9de3dbaf42bf00505647
timmy <timmy@ipca.com.tw>
2024-10-22 13:53:12 +0800

    update

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+## 使用Flask做前後端整合 ##
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
+# 加密密碼用
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
@@ -43,6 +45,7 @@ def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+        # 加密用
         hashed_password = generate_password_hash(password)
 
         cur = mysql.connection.cursor()
@@ -64,18 +67,30 @@ def change_password():
         cur = mysql.connection.cursor()
         cur.execute("SELECT password FROM users WHERE username = %s", (username,))
         result = cur.fetchone()
-
-        if result and check_password_hash(result[0], old_password):
-            hashed_new_password = generate_password_hash(new_password)
-            cur.execute("UPDATE users SET password = %s WHERE username = %s", (hashed_new_password, username))
-            mysql.connection.commit()
-            flash('密碼修改成功！', 'success')
+        # 先確認有此使用者
+        if result is None:
+            flash('使用者名稱不存在！', 'danger')
         else:
-            flash('舊密碼不正確！', 'danger')
-        
+            # 確認舊密碼不存在
+            if check_password_hash(result[0], old_password):
+                # 確認新舊密碼不同
+                if new_password == old_password:
+                    flash('舊密碼不得與新密碼相同', 'danger')
+                else:
+                    # 以上都成立 修改成功
+                    hashed_new_password = generate_password_hash(new_password)
+                    cur.execute("UPDATE users SET password = %s WHERE username = %s", (hashed_new_password, username))
+                    mysql.connection.commit()
+                    flash('密碼修改成功！', 'success')
+            else:
+                flash('舊密碼不正確！', 'danger')
+
         cur.close()
         return redirect(url_for('change_password'))
+    
     return render_template('change.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
